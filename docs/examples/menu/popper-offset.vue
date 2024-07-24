ef84328257cfb23db64433c09018ddc87acf9424
wukai139793595 <37068825+wukai139793595@users.noreply.github.com>
2024-07-24 09:49:20 +0800

    Update popper-offset.vue

--- docs/examples/menu/popper-offset.vue
+++ docs/examples/menu/popper-offset.vue
@@ -31,8 +31,20 @@
         <el-menu-item index="3-4-3">item three</el-menu-item>
       </el-sub-menu>
     </el-sub-menu>
-    <el-menu-item index="4" disabled>Info</el-menu-item>
-    <el-menu-item index="5">Orders</el-menu-item>
+    <el-sub-menu index="4" :popper-offset="8">
+      <template #title>Override Popper Offset</template>
+      <el-menu-item index="4-1">item one</el-menu-item>
+      <el-menu-item index="4-2">item two</el-menu-item>
+      <el-menu-item index="4-3">item three</el-menu-item>
+      <el-sub-menu index="4-4" :popper-offset="20">
+        <template #title>override child</template>
+        <el-menu-item index="4-4-1">item one</el-menu-item>
+        <el-menu-item index="4-4-2">item two</el-menu-item>
+        <el-menu-item index="4-4-3">item three</el-menu-item>
+      </el-sub-menu>
+    </el-sub-menu>
+    <el-menu-item index="6" disabled>Info</el-menu-item>
+    <el-menu-item index="7">Orders</el-menu-item>
   </el-menu>
 </template>
 
